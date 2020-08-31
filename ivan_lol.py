9d1ff07511184d46fa2d31d350c83a7dd7ae009a
Dima Cherenkov <cherenkov1123@gmail.com>
2020-08-31 18:08:08 +0300

    Second

--- ivan_lol.py
+++ ivan_lol.py
@@ -6,5 +6,16 @@ $ git status
 - посмотреть состояние репозитория
 $ git add .
 - добавить все файлы в репозиторий
-Файл .gitignore файлы, которые записаны в этом файле git будет игнорить
+Файл .gitignore : файлы, которые записаны в этом файле git будет игнорить
+$ git commit -m "SOME MESSAGE"
+$ git branch
+- показать все ветки
+$ git remote add origin link_to_repo
+- добавить удаленный гит репозиторий(называем его origin, можно по другому)
+$ git config --global user.name "YOUR_NAME"
+$ git config --global user.email "YOUR_EMAIL"
+- добавить почту и имя
+$ git push origin master
+- отправка текущей(активной) ветки на origin(удален репо) на ветку master(может быть другая)
+
 """
